aedcd5430799f0955caa2776d0e6decd46f0d098
DESKTOP-POEJTSP\osy04 <osy044@naver.com>
2023-08-10 16:43:09 +0900

    no message

--- 002_yolo/opencv_test.py
+++ 002_yolo/opencv_test.py
@@ -1,27 +1,28 @@
 import cv2
 
 
-
-
 cap = cv2.VideoCapture(0)
-
-
-sc, frame = cap.read()
-
 count = 0
 
+
+
 while True:
-    cv2.imshow("Frame", frame)
-    count += 1
-    print(count)
-
-
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
+    sc, frame = cap.read()
+    
+    if sc==True:
+        # frame resize
+        frame = cv2.resize(frame, dsize=(0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+        
+        # image show
+        cv2.imshow("Frame", frame)
+        count += 1
+        print(count)
+    
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 # close everything
 cap.release()
 k = cv2.waitKey(1)
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
